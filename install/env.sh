6847015d8e12951e1c6ac3ef5e143ecc97420f9e
Duncan Doyle <Duncan.Doyle@gmail.com>
2026-05-26 19:12:30 +0200

    Updated demo to ent-kgw 2.2.0.

--- install/env.sh
+++ install/env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export ENT_KGATEWAY_VERSION="2.2.0-beta.16"
+export ENT_KGATEWAY_VERSION="2.2.0"
 export ENT_KGATEWAY_HELM_VALUES_FILE="ent-kgateway-helm-values.yaml"
-export PORTAL_VERSION="2.2.0-beta.16"
-export K8S_GW_API_VERSION="v1.4.1"
+export PORTAL_VERSION="2.2.0"
+export K8S_GW_API_VERSION="v1.5.1"
 
 export ENT_KGATEWAY_SYSTEM_NAMESPACE="kgateway-system"
 export PORTAL_NAMESPACE="kgateway-system"
